3d8e13895c66f551c92bebdc59eb95d683eef91c
Hughc04 <161256307+Hughc04@users.noreply.github.com>
2024-02-25 21:52:19 +0000

    Add files via upload

---
+++ turtles.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 20:40:58 2024
+
+@author: hughc
+"""
+'turtle'
+
+# spirograph
+
+import math 
+import turtle
+
+def drawCircleTurtle(x, y, r):
+    # move to start circle
+    turtle.up()
+    turtle.setpos(x + r, y)
+    turtle.down()
+    
+    # draw circle
+    for i in range(0, 365, 5):
+        a = math.radians(i)
+        turtle.setpos(x + r*math.cos(a), y + r*math.sin(a))
+    
+drawCircleTurtle(100, 100, 50)
+turtle.mainloop()
+
+#%%
+
+'tutorial video'
+
+import turtle
+
+turtle.color('red', 'blue')
+
+turtle.begin_fill()
+
+def drawsquare
+
+
+turtle.end_fill()
+
+turtle.done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
